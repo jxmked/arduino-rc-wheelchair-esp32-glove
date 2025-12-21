e1c4eb3ea0572bb421892720d56d5c702d3c144d
jxmked <jovandeguia@gmail.com>
2025-12-22 01:34:35 +0800

    update

--- src/main.cpp
+++ src/main.cpp
@@ -10,34 +10,9 @@
 
 Gyro gyro;
 
-// BLEServer* pServer;
-// BLECharacteristic* pCharacteristic;
-// bool deviceConnected = false;
+TimeInterval update_hz(20, 0, true);
 
-// // Sensor variables
-// int16_t ax, ay, az;
-// int16_t gx, gy, gz;
-
-// // LED pin
-// #define LED_PIN 2
-
-// String lastCommand = "";
-// unsigned long lastSend = 0;
-// const unsigned long debounceTime = 200;  // ms
-
-// // --- BLE Callbacks ---
-// class MyServerCallbacks : public BLEServerCallbacks {
-//   void onConnect(BLEServer* pServer) {
-//     deviceConnected = true;
-//     digitalWrite(LED_PIN, HIGH);  // turn LED ON when connected
-//   };
-//   void onDisconnect(BLEServer* pServer) {
-//     deviceConnected = false;
-//     digitalWrite(LED_PIN, LOW);  // turn LED OFF when disconnected
-//   }
-// };
-
-TimeInterval blinkInterval(50, 0, true);
+bool is_connected = false;
 
 void setup() {
   Wire.begin();
@@ -48,38 +23,61 @@ void setup() {
 
   gyro.begin();
 
-  // pinMode(LED_PIN, OUTPUT);
-  // digitalWrite(LED_PIN, LOW);
-
-  // BLEDevice::init("GestureGlove");
-  // pServer = BLEDevice::createServer();
-  // pServer->setCallbacks(new MyServerCallbacks());
-
-  // BLEService* pService = pServer->createService(SERVICE_UUID);
-  // pCharacteristic = pService->createCharacteristic(
-  //     CHARACTERISTIC_UUID, BLECharacteristic::PROPERTY_NOTIFY |
-  //                              BLECharacteristic::PROPERTY_READ |
-  //                              BLECharacteristic::PROPERTY_WRITE);
-  // pCharacteristic->addDescriptor(new BLE2902());
-  // pService->start();
-  // pServer->getAdvertising()->start();
-
-  // Serial.println("Setup complete, waiting for gestures...");
+  is_connected = true;
 }
 
 void loop() {
-  // mpu.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
-
   gyro.loop();
 
-  if (blinkInterval.marked()) {
-    Serial.print("X : Y : Z | ");
-    Serial.print(gyro.x);
-    Serial.print(" : ");
-    Serial.print(gyro.y);
-    Serial.print(" : ");
-    Serial.println(gyro.z);
+  if (!is_connected) {
+    // Attempt to connect
+    return;
   }
+
+  if (!update_hz.marked()) return;
+
+  const float x = gyro.x;
+  const float y = gyro.y;
+
+  // Prevent sending the same command over and over again
+  static uint8_t last_command = 0x0;
+
+  uint8_t command = 0x0;  // We're only using 4-bit bitwise here. ;)
+
+  // Serial.print("X : Y : Z | ");
+  // Serial.print(gyro.x);
+  // Serial.print(" : ");
+  // Serial.print(gyro.y);
+  // Serial.print(" : ");
+  // Serial.println(gyro.z);
+
+  if (x >= 6500.0) {          // Left
+    command |= 0x8;           // 1000
+  } else if (x <= -4200.0) {  /// Right
+    command |= 0x4;           // 0100
+  } else {                    // Center
+    command &= ~0xC;          // 1100
+  }
+
+  if (y >= 4100.0) {          // Forward
+    command |= 0x1;           // 0001
+  } else if (y <= -4600.0) {  // Reverse
+    command |= 0x2;           // 0010
+  } else {                    // Center
+    command &= ~0x3;          // 0011
+  }
+
+  // Only send the new command. :)
+  if (last_command == command) return;
+  last_command = command;
+
+  Serial.println(command, BIN);
+  // Send Command
+
+  // Serial.print("BX : BY | ");
+  // Serial.print(bx);
+  // Serial.print(" : ");
+  // Serial.println(by);
 
   // String command = "";
 
