ecfef1d05541132743959eb53c945385944ae0d8
jxmked <jovandeguia@gmail.com>
2025-12-21 22:31:48 +0800

    update

--- src/constants.h
+++ src/constants.h
@@ -4,4 +4,7 @@
 #define SERVICE_UUID "c973126d-5afe-4aec-ab49-f8ffbd14f059"
 #define CHARACTERISTIC_UUID "9ca77c0d-14a4-403d-9677-0c2bf42b8194"
 
+#define LED_CONNECTED_PIN 3
+#define LED_ERROR_PIN 2
+
 #endif // CONSTANTS_H
